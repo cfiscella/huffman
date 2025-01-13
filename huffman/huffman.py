4363f0da88e2c5a19de63715563935086c27b113
cfiscella <chris2675@gmail.com>
2025-01-12 20:49:12 -0500

    ruff and black

--- huffman/huffman.py
+++ huffman/huffman.py
@@ -1,27 +1,28 @@
 from ds import btree, pqueue
 
+
 class HuffmanEncoder:
 
     def __init__(self, baseMessage):
         self.tree = self._make_tree(baseMessage)
         self.codex = self._build_codex(self.tree)
         self.encodedBaseMessage = self.encode(baseMessage)
-            
+
     def encode(self, message):
-        res = ''
+        res = ""
         for char in message:
-            res += self.codex['encoder'][char]
+            res += self.codex["encoder"][char]
         return res
 
     def decode(self, bitstring):
-        curr_b_str = ''
-        res = ''
-        decoder_codex = self.codex['decoder']
+        curr_b_str = ""
+        res = ""
+        decoder_codex = self.codex["decoder"]
         for char in bitstring:
             curr_b_str += char
             if curr_b_str in decoder_codex:
                 res += decoder_codex[curr_b_str]
-                curr_b_str = ''
+                curr_b_str = ""
         return res
 
     def _calc_frequencies(self, message):
@@ -34,12 +35,12 @@ class HuffmanEncoder:
                 freq[c] = [btree(c), 1]
         return freq
 
-    def _make_tree(self, message): 
-        #1. calculate frequencies
+    def _make_tree(self, message):
+        # 1. calculate frequencies
         frequencies = self._calc_frequencies(message)
-        #2. make priority queue
+        # 2. make priority queue
         q = pqueue(list(frequencies.values()), invert=True)
-        #3. build tree from bottom up
+        # 3. build tree from bottom up
         self.tree = btree()
         while len(q) > 1:
             # dqueue 2 smallest potential nodes
@@ -53,37 +54,36 @@ class HuffmanEncoder:
             q.add((nodesum, new_tree))
         final_node = q.poll().value
         return final_node
-    
+
     def _build_codex(self, tree):
-        curr_str = ''
-        codex = {'encoder': {}, 'decoder': {}}
+        curr_str = ""
+        codex = {"encoder": {}, "decoder": {}}
+
         def pre_walk(node, direction):
             nonlocal curr_str
             node_data = node.data
-            if direction == 'left':
-                curr_str += '0'
-            elif direction == 'right':
-                curr_str += '1'
-            if type(node_data) == str:
-                codex['encoder'][node_data] = curr_str
-                codex['decoder'][curr_str] = node_data
-        
+            if direction == "left":
+                curr_str += "0"
+            elif direction == "right":
+                curr_str += "1"
+            if isinstance(node_data, str):
+                codex["encoder"][node_data] = curr_str
+                codex["decoder"][curr_str] = node_data
+
         def post_walk(node, direction):
             nonlocal curr_str
-            node_data = node.data
-            if direction!= 'root':
+            if direction != "root":
                 curr_str = curr_str[:-1]
-                
+
         tree.dfs_walk(pre_walk_callback=pre_walk, post_walk_callback=post_walk)
         return codex
-        
-                
 
-if __name__ == '__main__':
-    string = 'BCAADDDCCACACAC'
+
+if __name__ == "__main__":
+    string = "BCAADDDCCACACAC"
     my_encoder = HuffmanEncoder(string)
     print(my_encoder.encodedBaseMessage)
     print(my_encoder.decode(my_encoder.encodedBaseMessage))
-    print('correct answer', string)
-    print('full lengh of stirng')
-    print(string==my_encoder.decode(my_encoder.encodedBaseMessage))
+    print("correct answer", string)
+    print("full lengh of stirng")
+    print(string == my_encoder.decode(my_encoder.encodedBaseMessage))
